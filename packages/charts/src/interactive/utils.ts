fc25c1f95343385b75a21847315ed7022de3d16e
Rostyslav Yurchyk <ros_ur@ukr.net>
2020-08-22 12:49:26 +0300

    add this.nodes check

--- packages/charts/src/interactive/utils.ts
+++ packages/charts/src/interactive/utils.ts
@@ -1,9 +1,4 @@
-import {
-    find,
-    isDefined,
-    isNotDefined,
-    mapObject,
-} from "../utils";
+import { find, isDefined, isNotDefined, mapObject } from "../utils";
 
 export function getValueFromOverride(override, index, key, defaultValue) {
     if (isDefined(override) && override.index === index) {
@@ -23,24 +18,31 @@ export function terminate() {
 export function saveNodeType(type) {
     return (node) => {
         // @ts-ignore
-        if (isNotDefined(node) && isDefined(this.nodes[type])) {
+        if (isDefined(this.nodes)) {
             // @ts-ignore
-            delete this.nodes[type];
+            if (isNotDefined(node) && isDefined(this.nodes[type])) {
+                // @ts-ignore
+                delete this.nodes[type];
+            } else {
+                // @ts-ignore
+                this.nodes[type] = node;
+            }
         } else {
             // @ts-ignore
-            this.nodes[type] = node;
+            this.nodes = [];
         }
-        // console.error(this.nodes)
     };
 }
 export function isHoverForInteractiveType(interactiveType) {
-    return function (moreProps) { // this has to be function as it is bound to this
+    return function (moreProps) {
+        // this has to be function as it is bound to this
 
         // @ts-ignore
         if (isDefined(this.nodes)) {
             // @ts-ignore
-            const selecedNodes = this.nodes
-                .map((node) => node.isHover(moreProps));
+            const selecedNodes = this.nodes.map((node) =>
+                node.isHover(moreProps)
+            );
             // @ts-ignore
             const interactive = this.props[interactiveType].map((t, idx) => {
                 return {
@@ -55,20 +57,20 @@ export function isHoverForInteractiveType(interactiveType) {
 
 export function isHover(moreProps) {
     // @ts-ignore
-    const hovering = mapObject(this.nodes, (node) => node.isHover(moreProps))
-        .reduce((a, b) => {
-            return a || b;
-        });
+    const hovering = mapObject(this.nodes, (node) =>
+        node.isHover(moreProps)
+    ).reduce((a, b) => {
+        return a || b;
+    });
     return hovering;
 }
 
 function getMouseXY(moreProps, [ox, oy]) {
     if (Array.isArray(moreProps.mouseXY)) {
-        const { mouseXY: [x, y] } = moreProps;
-        const mouseXY = [
-            x - ox,
-            y - oy,
-        ];
+        const {
+            mouseXY: [x, y],
+        } = moreProps;
+        const mouseXY = [x - ox, y - oy];
         return mouseXY;
     }
     return moreProps.mouseXY;
